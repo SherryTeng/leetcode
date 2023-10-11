3c9d91543808068723cc1ef496e6113e23aadd33
Sherry Teng <tengssthu@gmail.com>
2023-10-11 15:53:43 -0700

    new problem solved

---
+++ MinStack_155.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class MinStack_155 {
+    private Stack<int[]> stack= new Stack<>();
+    public MinStack_155() {
+
+    }
+
+    public void push(int val) {
+
+        if (stack.isEmpty()) {
+            stack.push(new int[] {val,val});
+        }else{
+            int min = stack.peek()[1];
+            stack.push(new int[] {val, Math.min(min, val)});
+        }
+    }
+
+    public void pop() {
+
+        stack.pop();
+    }
+
+    public int top() {
+
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+
+    }
+}
